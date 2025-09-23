8efa99dc416bb2999d0c0daecfd0c12f97aa3b7e
bhagwat-chate <bhagwat.chate25@gmail.com>
2025-09-23 09:05:31 +0530

    retriever

--- prod_assistant/retriever/retrieval.py
+++ prod_assistant/retriever/retrieval.py
@@ -20,7 +20,8 @@ class Retriever:
     def _load_env_variables(self):
         load_dotenv()
 
-        required_vars = ['OPENAI_API_KEY', 'GROQ_API_KEY', 'GOOGLE_API_KEY', 'ASTRA_DB_ENDPOINT', 'ASTRA_DB_APPLICATION_TOKEN', 'ASTRA_DB_KEYSPACE']
+        required_vars = ['OPENAI_API_KEY', 'GROQ_API_KEY', 'GOOGLE_API_KEY', 'ASTRA_DB_ENDPOINT',
+                         'ASTRA_DB_APPLICATION_TOKEN', 'ASTRA_DB_KEYSPACE']
 
         missing_vars = [var for var in required_vars if os.getenv(var) is None]
 
@@ -30,12 +31,50 @@ class Retriever:
         self.google_api_key = os.getenv('GOOGLE_API_KEY')
         self.openai_api_key = os.getenv('OPENAI_API_KEY')
         self.groq_api_key = os.getenv('GROQ_API_KEY')
-        self.db_aoi_endpoint = os.getenv('ASTRA_DB_ENDPOINT')
+        self.db_api_endpoint = os.getenv('ASTRA_DB_ENDPOINT')
         self.db_application_token = os.getenv('ASTRA_DB_APPLICATION_TOKEN')
         self.db_keyspace = os.getenv('ASTRA_DB_KEYSPACE')
 
     def load_retriever(self):
-        pass
+        """
+        Initialize and return a retriever from AstraDB Vector Store.
+
+        Returns:
+            VectorStoreRetriever: A retriever instance for semantic search.
+        """
+        if not self.vstore:
+            collection_name = self.config['astra_db']['collection_name']
+            self.vstore = AstraDBVectorStore(
+                embedding=self.model_loader.load_embeddings(),
+                collection_name=collection_name,
+                api_endpoint=self.db_api_endpoint,
+                token=self.db_application_token,
+                namespace=self.db_keyspace,
+            )
+
+        if not self.retriever:
+            retriever_config = self.config.get("retriever", {})
+            top_k = retriever_config.get("top_k", 3)
+            self.retriever = self.vstore.as_retriever(search_kwargs={"k": top_k})
+
+        return self.retriever
 
     def call_retriever(self, user_query: str):
-        pass
+        """
+        Run a query against the retriever and return matching documents.
+
+        Args:
+            user_query (str): The natural language query.
+
+        Returns:
+            List[Document]: List of relevant documents retrieved.
+        """
+        retriever = self.load_retriever()
+        return retriever.get_relevant_documents(user_query)
+
+
+if __name__ == '__main__':
+    retriever_obj = Retriever()
+    user_query = "iPhone 15"
+    output = retriever_obj.call_retriever(user_query)
+    print(output)
