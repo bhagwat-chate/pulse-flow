df643ad5590553d9e6f979685dc57045c7938eca
bhagwat-chate <bhagwat.chate25@gmail.com>
2025-09-23 08:24:02 +0530

    env var load for retriever

--- prod_assistant/retriever/retrieval.py
+++ prod_assistant/retriever/retrieval.py
@@ -6,18 +6,36 @@ from typing import List
 from langchain_core.documents import Document
 from prod_assistant.utils.model_loader import ModelLoader
 from dotenv import load_dotenv
+from prod_assistant.utils.config_loader import load_config
 
 
 class Retriever:
     def __init__(self):
-        pass
+        self.model_loader = ModelLoader()
+        self.config = load_config()
+        self._load_env_variables()
+        self.vstore = None
+        self.retriever = None
 
     def _load_env_variables(self):
-        pass
+        load_dotenv()
+
+        required_vars = ['OPENAI_API_KEY', 'GROQ_API_KEY', 'GOOGLE_API_KEY', 'ASTRA_DB_ENDPOINT', 'ASTRA_DB_APPLICATION_TOKEN', 'ASTRA_DB_KEYSPACE']
+
+        missing_vars = [var for var in required_vars if os.getenv(var) is None]
+
+        if missing_vars:
+            raise EnvironmentError(f"missing environment variables: {missing_vars}")
+
+        self.google_api_key = os.getenv('GOOGLE_API_KEY')
+        self.openai_api_key = os.getenv('OPENAI_API_KEY')
+        self.groq_api_key = os.getenv('GROQ_API_KEY')
+        self.db_aoi_endpoint = os.getenv('ASTRA_DB_ENDPOINT')
+        self.db_application_token = os.getenv('ASTRA_DB_APPLICATION_TOKEN')
+        self.db_keyspace = os.getenv('ASTRA_DB_KEYSPACE')
 
     def load_retriever(self):
         pass
 
-    def call_retriever(self):
+    def call_retriever(self, user_query: str):
         pass
-
