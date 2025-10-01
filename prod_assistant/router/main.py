f6f6dfb13c02fdfbcb06d7aa33934b16b461699e
bhagwat-chate <bhagwat.chate25@gmail.com>
2025-10-01 06:44:20 +0530

    api defined

--- prod_assistant/router/main.py
+++ prod_assistant/router/main.py
@@ -1,1 +1,35 @@
-print('test')
+import uvicorn
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from langchain_core.messages import HumanMessage
+from prod_assistant.workflow.agentic_workflow_with_mcp_websearch import AgenticRAG
+
+app = FastAPI()
+
+app.mount('/static', StaticFiles(directory='static'), name='static')
+templates = Jinja2Templates(directory='templates')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_methods=['*'],
+    allow_headers=['*'],
+    allow_credentials=True
+)
+
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("chat.html", {"request": request})
+
+
+@app.post("/get", response_class=HTMLResponse)
+async def chat(msg: str = Form(...)):
+    rag_agent = AgenticRAG()
+    answer = rag_agent.run(msg)
+    print(f"Agentic response: {answer}")
+
+    return answer
